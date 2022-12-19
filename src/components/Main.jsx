5609a8fa6102755467d7c2c3e733f643559efe4e
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-12-19 13:29:41 -0600

    added icons

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,11 +9,6 @@ import RDTmain from '../images/RDT_main2.png'
 import LkIn from '../images/LIInBug.png'
 import email from '../images/email.png'
 import git from '../images/github-mark-white.png'
-import express from '../images/express.png'
-import react from '../images/react.png'
-import js from '../images/js.png'
-import css from '../images/css.png'
-// import vue from '../images/vuejs-icon.png'
 
 const Main = () => {
   return (
@@ -38,16 +33,16 @@ const Main = () => {
       <p className="p-title">Projects</p>
       <div className="project-grid">
         <div className="project">
-          <Project title={'ResRecs'} image={RRmain} cap={"A restaurant recommendation app for small groups"} cap2={"ReactJS, MongoDB, JavaScript, HTML, CSS"} thumb={"https://resrecs.herokuapp.com"} git={"https://github.com/jscarpelli3/ResRecs"} git2Check={false} />
+          <Project expressCk={true} reactCk={true} jsCk={true} cssCk={true} vueCk={false} postgCk={true} htmlCk={true} mongoCk={true} title={'ResRecs'} image={RRmain} cap={"A restaurant recommendation app for small groups"} cap2={"ReactJS, MongoDB, JavaScript, HTML, CSS"} thumb={"https://resrecs.herokuapp.com"} git={"https://github.com/jscarpelli3/ResRecs"} git2Check={false} />
         </div>
         <div className="project">
-          <Project title={'RockDog Trivia!'} image={RDTmain} cap={"A mobile-styled competitive music trivia app"} thumb={"https://rockdog-trivia.herokuapp.com"} cap2={"VueJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/rockdog-trivia"} gitB={"https://github.com/jscarpelli3/rockdog-trivia-backend"} git2Check={true} />
-        </div>
-        <div>
-          <Project title={'The Maze Tower'} image={tmtScreen} cap={"A maze adventure game in the style of 80's MS-DOS Freeware"} thumb={"https://the-maze-tower.surge.sh/"} cap2={"JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Jon-Scarpelli_The-Maze-Tower"} git2Check={false} />
+          <Project expressCk={true} reactCk={false} jsCk={true} cssCk={true} vueCk={true} postgCk={true} htmlCk={true} mongoCk={false} title={'RockDog Trivia!'} image={RDTmain} cap={"A mobile-styled competitive music trivia app"} thumb={"https://rockdog-trivia.herokuapp.com"} cap2={"VueJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/rockdog-trivia"} gitB={"https://github.com/jscarpelli3/rockdog-trivia-backend"} git2Check={true} />
         </div>
         <div className="project">
-          <Project title={'Hired'} image={Hiredmain} cap={"A hiring app for bandleaders looking for musicians"} thumb={"https://hiredgig.herokuapp.com"} cap2={"ReactJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Hired"} gitB={"https://github.com/jscarpelli3/ResRecs"} git2Check={true} />
+          <Project expressCk={false} reactCk={false} jsCk={true} cssCk={true} vueCk={false} postgCk={false} htmlCk={true} mongoCk={false} title={'The Maze Tower'} image={tmtScreen} cap={"A maze adventure game in the style of 80's MS-DOS Freeware"} thumb={"https://the-maze-tower.surge.sh/"} cap2={"JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Jon-Scarpelli_The-Maze-Tower"} git2Check={false} />
+        </div>
+        <div className="project">
+          <Project expressCk={true} reactCk={true} jsCk={true} cssCk={true} vueCk={false} postgCk={true} htmlCk={true} mongoCk={false} title={'The Maze Tower'} image={tmtScreen} title={'Hired'} image={Hiredmain} cap={"A hiring app for bandleaders looking for musicians"} thumb={"https://hiredgig.herokuapp.com"} cap2={"ReactJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Hired"} gitB={"https://github.com/jscarpelli3/ResRecs"} git2Check={true} />
           {/* <div className="tech-box">
             <img className="tech" src={express} />
             <img className="tech" src={react} />
--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,31 @@
 import React from 'react'
 import gitIcn from '../images/github-mark-white.png'
+import express from '../images/express.png'
+import react from '../images/react.png'
+import js from '../images/js.png'
+import css from '../images/css.png'
+import vue from '../images/vuejs-icon.png'
+import postg from '../images/PostgreSQL.png'
+import html from '../images/html.png'
+import mongo from '../images/mongo.png'
 
-const Project = ({ title, image, cap, thumb, cap2, git, git2, git2Check }) => {
+const Project = ({ title, image, cap, thumb, cap2, git, git2, git2Check, expressCk, reactCk, jsCk, cssCk, vueCk, postgCk, htmlCk, mongoCk }) => {
   return (
     <div className="project-card" >
       <h1 className="project-title">{title}</h1>
       <a target="blank" href={thumb}>
         <img className="project-thumb" src={image} />
       </a>
+      <div className="tech-box">
+        {reactCk ? <img className="t-icon" src={react} /> : undefined}
+        {vueCk ? <img className="t-icon" src={vue} /> : undefined}
+        {expressCk ? <img className="t-icon" src={express} /> : undefined}
+        {mongoCk ? <img className="t-icon" src={mongo} /> : undefined}
+        {postgCk ? <img className="t-icon" src={postg} /> : undefined}
+        {htmlCk ? <img className="t-icon" src={html} /> : undefined}
+        {cssCk ? <img className="t-icon" src={css} /> : undefined}
+        {jsCk ? <img className="t-icon" src={js} /> : undefined}
+      </div>
       <div className="proj-details">
         <p className="caption">{cap}</p>
         <p className="caption">{cap2}</p>
