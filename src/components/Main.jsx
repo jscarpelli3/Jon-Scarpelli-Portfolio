2046abef1f146a1d40eda09cf096d4f99c3210c7
Jon Scarpelli <scarpelli.jon@gmail.com>
2022-12-19 10:15:55 -0600

    blub tweak

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -31,7 +31,7 @@ const Main = () => {
             <h3 className='hello'>hello, i'm</h3>
             <h1 className='name'>Jon Scarpelli</h1>
             <h4 className='titles'>Software Developer | Musician | Game Designer </h4>
-            <p className='about'>I am a Software engineer with a passion for creative problem solving and accessible design. Prior to transitioning to the tech world, I spent my career as a professional musician, music teacher and recording engineer where I developed the ability to bring together high level client service, clear communication skills & detail-oriented performance.</p>
+            <p className='about'>I am a Software engineer with a passion for creative problem solving and intuitive design. Prior to transitioning to the tech space, I spent my career as a professional musician, music teacher and recording engineer, cultivating my skill set in administering high level client service, detail-oriented deliverables & clear communication strategies.</p>
           </div>
         </div>
       </section >
@@ -41,7 +41,7 @@ const Main = () => {
           <Project title={'ResRecs'} image={RRmain} cap={"A restaurant recommendation app for small groups"} cap2={"ReactJS, MongoDB, JavaScript, HTML, CSS"} thumb={"https://resrecs.herokuapp.com"} git={"https://github.com/jscarpelli3/ResRecs"} git2Check={false} />
         </div>
         <div className="project">
-          <Project title={'RockDog Trivia!'} image={RDTmain} cap={"A mobile-styled competitive music trivia app"} thumb={"https://rockdog-trivia.herokuapp.com"} cap2={"VueJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/musicial_pursuit_front"} gitB={"https://github.com/jscarpelli3/musical_pursuit_server"} git2Check={true} />
+          <Project title={'RockDog Trivia!'} image={RDTmain} cap={"A mobile-styled competitive music trivia app"} thumb={"https://rockdog-trivia.herokuapp.com"} cap2={"VueJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/rockdog-trivia"} gitB={"https://github.com/jscarpelli3/rockdog-trivia-backend"} git2Check={true} />
         </div>
         <div>
           <Project title={'The Maze Tower'} image={tmtScreen} cap={"A maze adventure game in the style of 80's MS-DOS Freeware"} thumb={"https://the-maze-tower.surge.sh/"} cap2={"JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Jon-Scarpelli_The-Maze-Tower"} git2Check={false} />
