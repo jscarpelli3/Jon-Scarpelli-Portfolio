f1817f6d0586f34c9d4760ec264e7150dc518453
Jon Scarpelli <scarpelli.jon@gmail.com>
2023-01-10 14:01:27 -0600

    re-ordered h-tags. changes cap2 to provide mroe context

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -22,39 +22,46 @@ const Main = () => {
 
         </div>
         <div className="greeting">
-          <img alt="prof-pic" className="prof-pic" src={jsAltPic}></img>
+          <div>
+            <img alt="prof-pic" className="prof-pic" src={jsAltPic}></img>
+            <div className="more-about">
+              <a href="https://docs.google.com/document/d/1aQ81_zmerbjSuLIKYvzh8fgONmQjI4K6ngtQvaX414Q/export?format=pdf" target="blank">
+                <h3>DOWNLOAD MY RESUME</h3>
+              </a>
+            </div>
+          </div>
           <div className='greeting-info'>
-            <h3 className='hello'>hello, i'm</h3>
+            <h1 className='hello'>hello, i'm</h1>
             <h2 className='name'>Jon Scarpelli</h2>
-            <h4 className='titles'>Software Developer | Musician | Game Designer </h4>
+            <h3 className='titles'>Software Developer | Musician | Game Designer </h3>
             <p className='about'>I am a Software engineer with a passion for creative problem solving and intuitive design. Prior to transitioning to the tech space, I spent my career as a professional musician, music teacher and recording engineer, cultivating my skill set in administering high level client service, detail-oriented deliverables & clear communication strategies.</p>
             <Link to="/about-me">
               <h4 className="more">...click for more about me</h4>
             </Link>
           </div>
-          <div className="more-about">
-            {/* <a href="https://docs.google.com/document/d/1aQ81_zmerbjSuLIKYvzh8fgONmQjI4K6ngtQvaX414Q/export?format=pdf" target="blank">
-              <h4>dowload my resume here</h4>
-            </a> */}
-          </div>
+          {/* <div className="more-about">
+            <a href="https://docs.google.com/document/d/1aQ81_zmerbjSuLIKYvzh8fgONmQjI4K6ngtQvaX414Q/export?format=pdf" target="blank">
+              <h3>DOWNLOAD MY RESUME</h3>
+            </a>
+          </div> */}
         </div>
       </section >
       <h2 className="p-title">Projects</h2>
       <div className="project-grid">
         <div className="project">
-          <Project expressCk={true} reactCk={true} jsCk={true} cssCk={true} vueCk={false} postgCk={false} htmlCk={true} mongoCk={true} title={'ResRecs'} image={RRmain} cap={"A restaurant recommendation app for small groups"} cap2={"ReactJS, MongoDB, JavaScript, HTML, CSS"} thumb={"https://resrecs.herokuapp.com"} git={"https://github.com/jscarpelli3/ResRecs"} git2Check={false} />
+          <Project expressCk={true} reactCk={true} jsCk={true} cssCk={true} vueCk={false} postgCk={false} htmlCk={true} mongoCk={true} title={'ResRecs'} image={RRmain} cap={"A restaurant recommendation app for small groups"} cap2={"note: server code lives in the same Git repo as the client"} thumb={"https://resrecs.herokuapp.com"} git={"https://github.com/jscarpelli3/ResRecs"} git2Check={false} />
         </div>
         <div className="project">
-          <Project expressCk={true} reactCk={false} jsCk={true} cssCk={true} vueCk={true} postgCk={true} htmlCk={true} mongoCk={false} title={'RockDog Trivia!'} image={RDTmain} cap={"A mobile-styled competitive music trivia app"} thumb={"https://rockdog-trivia.herokuapp.com"} cap2={"VueJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/rockdog-trivia"} gitB={"https://github.com/jscarpelli3/rockdog-trivia-backend"} git2Check={true} />
+          <Project expressCk={true} reactCk={false} jsCk={true} cssCk={true} vueCk={true} postgCk={true} htmlCk={true} mongoCk={false} title={'RockDog Trivia!'} image={RDTmain} cap={"A mobile-styled competitive music trivia app"} thumb={"https://rockdog-trivia.herokuapp.com"} cap2={"Meant for mobile devices & features hand-rolled auth and a 3rd party API"} git={"https://github.com/jscarpelli3/rockdog-trivia"} gitB={"https://github.com/jscarpelli3/rockdog-trivia-backend"} git2Check={true} />
         </div>
         <div className="project">
-          <Project expressCk={false} reactCk={false} jsCk={true} cssCk={true} vueCk={false} postgCk={false} htmlCk={true} mongoCk={false} title={'The Maze Tower'} image={tmtScreen} cap={"A maze adventure game in the style of 80's MS-DOS Freeware"} thumb={"https://the-maze-tower.surge.sh/"} cap2={"JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Jon-Scarpelli_The-Maze-Tower"} git2Check={false} />
+          <Project expressCk={false} reactCk={false} jsCk={true} cssCk={true} vueCk={false} postgCk={false} htmlCk={true} mongoCk={false} title={'The Maze Tower'} image={tmtScreen} cap={"A maze adventure game in the style of 80's MS-DOS Freeware"} thumb={"https://the-maze-tower.surge.sh/"} cap2={"This is an original game design featuring modular level building at runtime"} git={"https://github.com/jscarpelli3/Jon-Scarpelli_The-Maze-Tower"} git2Check={false} />
         </div>
         <div className="project">
-          <Project expressCk={true} reactCk={true} jsCk={true} cssCk={true} vueCk={false} postgCk={true} htmlCk={true} mongoCk={false} title={'Hired'} image={Hiredmain} cap={"A hiring app for bandleaders looking for musicians"} thumb={"https://hiredgig.herokuapp.com"} cap2={"ReactJS, PostgreSQL, JavaScript, HTML, CSS"} git={"https://github.com/jscarpelli3/Hired"} gitB={"https://github.com/jscarpelli3/ResRecs"} git2Check={true} />
+          <Project expressCk={true} reactCk={true} jsCk={true} cssCk={true} vueCk={false} postgCk={true} htmlCk={true} mongoCk={false} title={'Hired'} image={Hiredmain} cap={"A hiring app for bandleaders looking for musicians"} thumb={"https://hiredgig.herokuapp.com"} cap2={"Includes hand-rolled auth"} git={"https://github.com/jscarpelli3/Hired"} gitB={"https://github.com/jscarpelli3/ResRecs"} git2Check={true} />
         </div>
         <div className="project">
-          <Project expressCk={false} reactCk={false} jsCk={false} cssCk={false} vueCk={false} postgCk={false} htmlCk={false} mongoCk={false} pythonCk={true} title={'Python Adventure'} image={zmbScreen} cap={"A 'Choose Your Own Adventure' written in python"} thumb={"https://replit.com/@jscarpelli3/zombie-text-adventure"} cap2={"Text-only"} git={"https://github.com/jscarpelli3/zombie-text-adventure"} git2Check={false} />
+          <Project expressCk={false} reactCk={false} jsCk={false} cssCk={false} vueCk={false} postgCk={false} htmlCk={false} mongoCk={false} pythonCk={true} title={'Python Adventure'} image={zmbScreen} cap={"A 'Choose Your Own Adventure' written in Python"} thumb={"https://replit.com/@jscarpelli3/zombie-text-adventure"} cap2={"Text-only & features 8 different endings."} git={"https://github.com/jscarpelli3/zombie-text-adventure"} git2Check={false} />
         </div>
       </div>
     </div >
