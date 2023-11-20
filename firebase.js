66cfa754fef3ea934fea65c7450f1a771cffed9c
jscarpelli3 <scarpelli.jon@gmail.com>
2023-11-20 15:07:51 -0600

    deploy 18

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+	apiKey: "AIzaSyBJAJdt5PbSU1_Gx4aGI0NisJfu7cHXGLU",
+	authDomain: "jscarpelli-home.firebaseapp.com",
+	projectId: "jscarpelli-home",
+	storageBucket: "jscarpelli-home.appspot.com",
+	messagingSenderId: "1072720232081",
+	appId: "1:1072720232081:web:023d9f4bec5f978dfd0930",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
